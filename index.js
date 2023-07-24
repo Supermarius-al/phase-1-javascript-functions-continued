2becbd70542d6aa65ebef1eb779e687516cf52d1
Marius Alban <malban298@gmail.com>
2023-07-24 11:28:33 -0300

    why was this so hard

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // code your solution here
+function saturdayFun(activity) {
+  if (activity == undefined) {
+    return `This Saturday, I want to roller-skate!`;
+  } else {
+    return `This Saturday, I want to ${activity}!`;
+  }
+}
+
+const mondayWork = function (activity) {
+  if (activity == undefined) {
+    return `This Monday, I will go to the office.`;
+  } else {
+    return `This Monday, I will ${activity}.`;
+  }
+};
+function wrapAdjective(specialAdjective) {
+  return function innerFunction(adjective) {
+    return String(
+      `You are ${specialAdjective}${adjective}${specialAdjective}!`
+    );
+  };
+}
